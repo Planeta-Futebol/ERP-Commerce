ce6115b02bb508807a316a28d33dc72b2c7b3ae3
Ziru Zanrgeiff <ronildo_net@hotmail.com>
2016-10-24 16:28:32 -0200

    Add grid to import product

--- app/code/Planet/Product/Block/Adminhtml/Main.php
+++ app/code/Planet/Product/Block/Adminhtml/Main.php
@@ -2,9 +2,38 @@
 
 namespace Planet\Product\Block\Adminhtml;
 
-class Main extends \Magento\Backend\Block\Template
+class Main extends \Magento\Backend\Block\Widget\Container
 {
+
+    public function __construct(
+        \Magento\Backend\Block\Widget\Context $context,
+        array $data = []
+    ){
+        parent::__construct($context, $data);
+    }
+    protected function _construct()
+    {
+        $this->_blockGroup = 'Planet_Product';
+        $this->_controller = 'adminhtml_index';
+
+        parent::_construct();
+    }
+
     function _prepareLayout(){
 
+        $this->setChild(
+            'grid',
+            $this->getLayout()->createBlock(
+                'Planet\Product\Block\Adminhtml\Import\Grid',
+                'agent.import.grid'
+            )
+        );
+
+        return parent::_prepareLayout();
+    }
+
+    public function getGridHtml()
+    {
+        return $this->getChildHtml('grid');
     }
 }
